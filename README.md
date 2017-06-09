dbc7c7252be1a5e3f925f06288247dcdab358b4c
benpptung <benpptung@tacol.biz>
2017-06-09 12:12:54 +0800

    add deep merge feature

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ send new props to children in React.
 
 ## Installation
 
-`npm install -D react-children-with`
+`npm install -S react-children-with`
 
 ## Example
 
 Instead of
 
 ```
+const React = require('react');
 
 var children = React.Children.map(this.props.children, child=> {
 
@@ -21,12 +22,16 @@ var children = React.Children.map(this.props.children, child=> {
 
 Just write
 ```
+const merge = require('react-children-with');
+
 var children = merge(this.props.children, new_props);
 
 ```
 
 or Instead of
 ```
+const React = require('react');
+
 var children = React.Children.map(this.props.children, child=> {
   return [Apple, Orange].indexOf(child.type) >= 0 ? 
     React.cloneElement(child, new_props) : child;
@@ -35,5 +40,29 @@ var children = React.Children.map(this.props.children, child=> {
 
 just write
 ```
+const merge = require('react-children-with');
+
 var children = merge(this.props.children, new_props, [Apple, Orange]);
+```
+
+## merge.deep()
+same as merge(), but it can merge props to grand child, or grand's grand child....
+
+```
+<Parent>
+	<Child>
+		<GrandChild />
+	</Child>
+	<Child>
+		<GrandChild />
+	</Child>
+</Parent>
+```
+
+in Parent, merge new_props to `GrandChild` and skip `Child`
+```
+const merge = require('react-children-with').deep;
+
+var children = merge(props.children, {onSelect}, [GrandChild]);
+
 ```
--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const React = require('react');
  * @param children
  * @param props
  * @param type
+ * @param deep
  * @return {ReactElement}
  */
-module.exports = function (children, props, type) {
+exports = module.exports = merge;
+function merge (children, props, type, deep) {
 
   var _props = {};
   var has_prop = false;
@@ -21,22 +23,40 @@ module.exports = function (children, props, type) {
   }
 
   if (!has_prop) return children;
-  return React.Children.map(children, child=> {
+  if (typeof type == 'boolean' ) deep = type;
 
-    // ignore html element
-    if (typeof child.type == 'string') return child;
+  return mergeProps(children);
+  function mergeProps(children) {
 
-    // if type, only limited types recieve new props
-    if (typeof type == 'function') {
-      return child.type === type ? React.cloneElement(child, _props) : child;
-    }
+    return React.Children.map(children, child=> {
 
-    // if type is an Array, check if child is in the array, and add new props
-    if (Array.isArray(type)) {
-      return type.indexOf(child.type) >= 0 ? React.cloneElement(child, _props) : child;
-    }
+      let __props = _props;
+      let new_children = null;
 
-    // if no type, all children recieve new props
-    return React.cloneElement(child, _props);
-  })
+      if (child.props && React.Children.count(child.props.children) > 0 && deep === true) {
+        new_children = {children: mergeProps(child.props.children) };
+        __props = Object.assign({}, _props, new_children );
+      }
+
+      if (typeof child.type == 'string') return new_children ? React.cloneElement(child, new_children) : child;
+
+      // if type is a constructor
+      if (typeof type == 'function') {
+        return child.type === type ? React.cloneElement(child, __props) :
+          new_children ? React.cloneElement(child, new_children) : child;
+      }
+
+      // if type is an array with constructors included
+      if (Array.isArray(type)) {
+        return type.indexOf(child.type) >= 0 ? React.cloneElement(child, __props) :
+          new_children ? React.cloneElement(child, new_children) : child;
+      }
+
+      return React.cloneElement(child, __props);
+    })
+  }
+}
+
+exports.deep = function(children, props, type) {
+  return merge(children, props, type, true);
 };
